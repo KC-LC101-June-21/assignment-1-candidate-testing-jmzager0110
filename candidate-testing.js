f9ac87739807d3bf12b883e8a0a714ed9a0c6592
jmzager <84881009+jmzager0110@users.noreply.github.com>
2021-06-10 23:51:27 +0000

    Fixed all of the issues I created, moved my code to the correct places, tested. JZ

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,47 +3,46 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName = ();
+let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question = ("Who was the first American woman in space? ");
 let correctAnswer = ("Sally Ride");
-let candidateAnswer = ();
+let candidateAnswer = "";
 let questions;
 let correctAnswers;
 let candidateAnswers;
 
 
-function askForName(let candidateName = input.question("What is your name?")); {
+function askForName(candidateName = input.question("What is your name?")) {
   // TODO 1.1b: Ask for candidate's name //
 
 }
-let candidateName = input.question("What is your name?");
-function askQuestion(let candidateAnswer = input.question(question)); {
+
+function askQuestion(candidateAnswer = input.question(question)) {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
 
 
 }
-let candidateAnswer = input.question(question);
+
 
 //Future Steps
-function gradeQuiz(candidateAnswers) {
+function gradeQuiz() {
 
   let grade;
   
   return grade;
 }
 // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-if candidateAnswer === correctAnswer {
-  console.log("That is correct!");
-} else {
-  console.log("That is not correct!");
-}
-
+if (candidateAnswer = correctAnswer) {
+   console.log("That is correct!");
+ } else {
+   console.log("That is not correct!");
+ }
+  // TODO 1.1c: Ask for candidate's name //
+console.log("Hello " + candidateName + "!");
 function runProgram() {
   askForName();
-  // TODO 1.1c: Ask for candidate's name //
-  //Shouldn't 1.1.c Be the greeting response to the candidate entering their name?
-console.log("Hello " + candidateName + "1");
+
 
   askQuestion();
   gradeQuiz(this.candidateAnswers);

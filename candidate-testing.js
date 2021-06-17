5ec5a067bbc478a197ac9ec24ae91dfa8403046a
jmzager <84881009+jmzager0110@users.noreply.github.com>
2021-06-17 00:20:20 +0000

    Finally Part 2!

--- candidate-testing.js
+++ candidate-testing.js
@@ -5,40 +5,68 @@ const input = require('readline-sync');
 // TODO 1.1a: Define candidateName // 
 let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question = "Who was the first American woman in space? ";
-let correctAnswer = "Sally Ride";
-let candidateAnswer = "";
-let questions
-let correctAnswers
-let candidateAnswers
+let question;
+let correctAnswer;
+let candidateAnswer;
+let questions = ["Who was the first American woman in space?  ", 
+"True or false: 5 kilometer == 5000 meters?   ", "(5+3)/2*10=?   ", 
+"Given the array ['8', 'Orbit', 'Trajectory', '45'], what entry is at index 2?   ", 
+"What is the minimum crew size for the ISS?   "];
+let correctAnswers = ['Sally Ride', 'true', '40', 'Trajectory', '3'];
+let candidateAnswers = [];
 
 
 
-function askForName(candidateName) {
-  // TODO 1.1b: Ask for candidate's name //
-let userResponse = input.question("What is your name?");
 
-return userResponse;
+function askForName() {
+  // TODO 1.1b: Ask for candidate's name // This needs to be corrected for final part!
+  
+  candidateName = input.question("What is your name?  ");
+
+return candidateName;
 }
 
-function askQuestion(candidateAnswers) { 
-    // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-    (candidateAnswer = input.question(question)); 
-   
-
+function askQuestion() { 
+    // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer 
+  for (let i = 0; i < questions.length; i++) {
+    let tempAnswer = input.question(questions[i]);
+    candidateAnswers.push(tempAnswer);
+    
+   } 
 }
 
 function gradeQuiz(candidateAnswers) {
 
 // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly 
- if (candidateAnswer === correctAnswer) {
-   console.log("That is correct!");
- } else {
-   console.log("That is not correct!");
- }
+// Replace basic feedback w/ template literal that disp each respose w/ the corresponding correct answer
+
+console.log
+(`
+Candidate Name: ${candidateName} 
+1) ${questions[0]}
+Your answer: ${candidateAnswers[0]}
+Correct Answer: ${correctAnswers[0]}
+
+2) ${questions[1]}
+Your Answer: ${candidateAnswers[1]}
+Correct Answer: ${correctAnswers[1]}
+
+3) ${questions[2]}
+Your Answer: ${candidateAnswers[2]}
+Correct Answer: ${correctAnswers[2]}
+
+4) ${questions[3]}
+Your Answer: ${candidateAnswers[3]}
+Correct Answer: ${correctAnswers[3]}
+
+5) ${questions[4]}
+Your Answer: ${candidateAnswers[4]}
+Correct Answer: ${correctAnswers[4]}`);
+
 
   
   let grade;
+  //most of your code between grade and return grade
   
 
   return grade;
@@ -47,9 +75,8 @@ function gradeQuiz(candidateAnswers) {
 
 
 function runProgram() {
-  
-  // TODO 1.1c: Ask for candidate's name //
-  console.log("Hello, "  +(askForName()) + "!");
+  //TODO 1.1c: Ask for candidate's name //
+  console.log(askForName());
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
